Fix logistic regression, there is still an issue with error calculation

log_reg.py
@@ -8,7 +8,7 @@ class Model():
 
 	def forward(self, x):
 		"""Allow for batch processing, assume row-major"""
-		return self.sigmoid(np.dot(x, self.w))
+		return np.round(self.sigmoid(np.dot(x, self.w)))
 
 	def sigmoid(self, x):
 		return 1 / (1 + np.exp(-x))
@@ -17,16 +17,17 @@ class Model():
 		"""Not used in weight update, only for human consumption (and please do so moderately)"""
 		"""NOTE as forward(x) approaches 0 or 1, floating point approximationg + log may **** this up"""
 		# return np.sum(y * np.log(np.maximum(self.forward(x),[0.000001])) + (1-y)*np.log(np.maximum(1-self.forward(x), [0.0000001]))) / x.shape[1]
-		return np.sum(y * np.log(self.forward(x)) + (1-y)*np.log(1-self.forward(x))) / x.shape[1]
+		y = y.reshape(y.shape[0],1).T
+		return np.dot(y, np.log(self.forward(x) + 0.000001)) + np.dot((1-y), np.log(1-self.forward(x) + 0.000001)) / x.shape[0]
 
 	def gradient(self, x, y):
-		return x * (y - self.forward(x))
+		# print x.shape, (y-self.forward(x)).shape, self.w.shape
+		return np.dot((y - self.forward(x)), x)
 
 	def step(self, x, y, e=float("inf")):
 		"""By default, this function performs one update step
 			error epsilon can be changed to perform multi updates at once"""
-		while self.error(x,y) > e:
-			self.w += self.learning_rate * np.sum(self.gradient(x,y), axis=0).reshape(3,1)
+		self.w += self.learning_rate * np.sum(self.gradient(x,y), axis=0).T.reshape(self.input_dim, 1)
 
 	def save(self, filename):
 		np.save(filename, self.w, allow_pickle=False)

log_reg_test.py
@@ -13,14 +13,29 @@ cov_mat2 = np.array([[1,0],[0,1]])
 x2_samples = np.random.multivariate_normal(mu_vec2, cov_mat2, 100)
 mu_vec2 = mu_vec2.reshape(1,2).T
 
-
-fig = plt.figure()
-
-
 plt.scatter(x1_samples[:,0],x1_samples[:,1], marker='+')
 plt.scatter(x2_samples[:,0],x2_samples[:,1], c= 'green', marker='o')
 
 X = np.concatenate((x1_samples,x2_samples), axis = 0)
 Y = np.array([0]*100 + [1]*100)
+
+model = log_reg.Model(2, 0.1)
+
+for i in range(10):
+	model.step(X,Y)
+	print 'Iteration {} with error: {}'.format(i, model.error(X,Y))
+
+# X - some data in 2dimensional np.array
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),np.arange(y_min, y_max, 0.1))
+# here "model" is your model's prediction (classification) function
+Z = model.forward(np.c_[xx.ravel(), yy.ravel()])
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+plt.axis('off')
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
 
 plt.show()